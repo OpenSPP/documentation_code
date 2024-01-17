8e8c92f51d45bd4009106bb1db82d153445e2a62
Dasun Hegoda <dasunhegoda@gmail.com>
2024-01-17 16:33:20 +0530

    rest_api

---
+++ howto/developer_guides/customizations/spp_api_records_custom/__manifest__.py
@@ -0,0 +1,24 @@
+# Part of OpenSPP. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenSPP REST API: API Records Custom",
+    "category": "OpenSPP",
+    "version": "15.0.1.1.0",
+    "sequence": 1,
+    "author": "OpenSPP.org",
+    "website": "https://github.com/openspp/openspp-modules",
+    "license": "LGPL-3",
+    "development_status": "Alpha",
+    "maintainers": ["openspp_dev"],
+    "depends": [
+        "spp_api_records",
+    ],
+    "data": [
+        "data/spp_api_path_data.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
