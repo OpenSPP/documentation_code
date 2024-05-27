bca68d755eeae5a704dc9466bfee723bfb71f96a
Red Butay <35404367+reichie020212@users.noreply.github.com>
2024-05-27 18:21:33 +0800

    Update group.py
    removed api.depends to the compute function and added argument store=True to the field

--- howto/developer_guides/customizations/spp_registry_custom/models/group.py
+++ howto/developer_guides/customizations/spp_registry_custom/models/group.py
@@ -7,9 +7,9 @@ class OpenSPPGroupCustom(models.Model):
     z_ind_grp_count_below_salary = fields.Integer(
         "Number of members with below 100 salary",
         compute="_compute_count_below_salary",
+        store=True,
     )
 
-    @api.depends("group_membership_ids", "group_membership_ids.salary")
     def _compute_count_below_salary(self):
         for rec in self:
             below_salary_count = 0
